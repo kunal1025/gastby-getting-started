ec782ad1d28d0eebbd0236e88e03e6b295df3d41
Kunal Patel <kunalp1025@gmail.com>
2024-04-01 20:20:31 -0400

    create gatsby site

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-getting-started`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
